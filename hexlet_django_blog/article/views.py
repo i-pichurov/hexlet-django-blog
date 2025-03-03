7da2229211f350532f3133b76fb1dbe3a2665bdf
i-pichurov <deadbassein@gmail.com>
2025-03-03 23:14:20 +0300

    add new templates

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -3,4 +3,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('article')
+    return render(request, 'articles/index.html', context={
+        'title': 'ARTICLE',
+    })
